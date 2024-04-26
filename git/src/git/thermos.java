90a30e49734776086d6545b0c1239c553198b838
aicasetools <aishwaryashirodkar06@gmail.com>
2024-04-27 00:06:59 +0530

    first commit

---
+++ git/src/git/thermos.java
@@ -0,0 +1,18 @@
+package git;
+
+public class thermos {
+
+	
+	String brand;
+	String liquid;
+	
+	public thermos() {
+		this.brand=brand;
+		this.liquid=liquid;
+	}
+	
+	String getaddthermos() {
+		return brand;
+	}
+	
+}
